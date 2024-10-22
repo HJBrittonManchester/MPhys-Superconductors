f88c9f4f7d03e82f2a192f719a25e48cba8584ef
hazza <57954412+Hbritton@users.noreply.github.com>
2024-10-22 11:16:26 +0100

    collected data for 3meV

--- Susceptibility.py
+++ Susceptibility.py
@@ -12,6 +12,7 @@ import matplotlib.colors as colors
 import scipy.constants
 import time
 
+beta = 1.00730995e+01  # for 3 mev band gap at fermi surface
 
 #alpha_rashba *= 5
 #alpha_zeeman = 0
@@ -25,8 +26,8 @@ k_B = scipy.constants.physical_constants["Boltzmann constant in eV/K"][0]
 V = -0.34864337758262043
 
 # simulation params
-FREQUENCY_LIMIT = 4000
-DEFAULT_N_POINTS = 200
+FREQUENCY_LIMIT = 10000
+DEFAULT_N_POINTS = 1000
 
 
 def Det(kx, ky, omega, H):
@@ -56,7 +57,7 @@ def matsubara_frequency(T, m):
     return (2*m+1)*k_B * T * np.pi
 
 
-def Susceptibility(T, H_mag, theta=np.pi/2, phi=0, plot=False, N_points=DEFAULT_N_POINTS, threshold=2):
+def Susceptibility(T, H_mag, theta=np.pi/2, phi=0, plot=False, N_points=DEFAULT_N_POINTS, threshold=.01):
     b2_range = np.linspace(0, 1, N_points)
     b1_range = np.linspace(0, 1, N_points)
 
@@ -83,14 +84,15 @@ def Susceptibility(T, H_mag, theta=np.pi/2, phi=0, plot=False, N_points=DEFAULT_
     # to plot energy
     if plot:
         # define your scale, with white at zero
-        norm = colors.CenteredNorm()
+        #norm = colors.CenteredNorm()
         #colors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
 
         c = plt.pcolormesh(KX, KY, epsilon(KX, KY),
-                           shading='auto', cmap='RdYlBu', norm=norm)
+                           shading='auto', cmap='RdYlBu')
         cbar = plt.colorbar(c)
         cbar.set_label("Energy")
-        #plt.scatter(invalid_kx, invalid_ky, color="k", label = "excluded k-points")
+        plt.scatter(valid_kx, valid_ky, color="k",
+                    label="excluded k-points")
         plt.ylabel(r"$k_y$")
         plt.xlabel(r"$k_x$")
         plt.plot(1/3 * (4 / np.sqrt(3) * np.pi / a + 2 /
@@ -100,6 +102,7 @@ def Susceptibility(T, H_mag, theta=np.pi/2, phi=0, plot=False, N_points=DEFAULT_
 
         plt.plot(0, 4/3 * np.pi / a, 'xk')
         plt.text(.04,  4/3 * np.pi / a + .04, r"$K$")
+        print(valid_kx.shape)
 
     # print(valid_kx.shape)
 
@@ -116,22 +119,23 @@ def Susceptibility(T, H_mag, theta=np.pi/2, phi=0, plot=False, N_points=DEFAULT_
 
             freq_block[2*m +
                        1] = matsubara_frequency(T, -(m + block_step * block_size))
-        GF_part -= np.real((GF_up_up(valid_kx[:, None], valid_ky[:, None], freq_block[None, :], H_mag) * GF_down_down(-valid_kx[:, None], -valid_ky[:, None], -freq_block[None, :], H_mag)- GF_up_down(valid_kx[:, None], valid_ky[:, None], freq_block[None, :], H_mag) * GF_down_up(-valid_kx[:, None], -valid_ky[:, None], -freq_block[None, :], H_mag) ).sum(axis=1))
+        GF_part -= np.real((GF_up_up(valid_kx[:, None], valid_ky[:, None], freq_block[None, :], H_mag) * GF_down_down(-valid_kx[:, None], -valid_ky[:, None], -freq_block[None, :], H_mag) - GF_up_down(
+            valid_kx[:, None], valid_ky[:, None], freq_block[None, :], H_mag) * GF_down_up(-valid_kx[:, None], -valid_ky[:, None], -freq_block[None, :], H_mag)).sum(axis=1))
 
     chi_0 = GF_part * T * k_B / (N_points**2)
 
     # to plot susc
     #trip = plt.tripcolor(valid_kx, valid_ky, chi_0)
-    #plt.colorbar(trip)
+    # plt.colorbar(trip)
 
     return chi_0.sum()
 
 
-def delta(T, H_mag, theta=np.pi/2, phi=0., N_points = DEFAULT_N_POINTS):
-    return 1 - Susceptibility(T, H_mag, theta, phi, N_points = N_points) * V
+def delta(T, H_mag, theta=np.pi/2, phi=0., N_points=DEFAULT_N_POINTS):
+    return 1 - Susceptibility(T, H_mag, theta, phi, N_points=N_points) * V
 
 
-def braket(start_H_U, start_H_L, T, theta=np.pi/2, phi=0., tol=0.001, N_points = DEFAULT_N_POINTS):
+def braket(start_H_U, start_H_L, T, theta=np.pi/2, phi=0., tol=0.0001, N_points=DEFAULT_N_POINTS):
 
     # Larger H => +ve Delta
     # Smaller H => -ve Delta
@@ -141,8 +145,8 @@ def braket(start_H_U, start_H_L, T, theta=np.pi/2, phi=0., tol=0.001, N_points =
     current_H_U = start_H_U
     current_H_L = start_H_L
 
-    current_delta_U = delta(T, current_H_U, theta, phi, N_points =N_points)
-    current_delta_L = delta(T, current_H_L, theta, phi, N_points =N_points)
+    current_delta_U = delta(T, current_H_U, theta, phi, N_points=N_points)
+    current_delta_L = delta(T, current_H_L, theta, phi, N_points=N_points)
 
     # print("Δ_max = {}, Δ_min = {}".format(
     #     current_delta_U, current_delta_L))
@@ -170,7 +174,8 @@ def braket(start_H_U, start_H_L, T, theta=np.pi/2, phi=0., tol=0.001, N_points =
             # reset upper
             current_delta_U = current_delta_L
             # recalculate lower
-            current_delta_L = delta(T, current_H_L, theta, phi, N_points =N_points)
+            current_delta_L = delta(
+                T, current_H_L, theta, phi, N_points=N_points)
 
         elif current_delta_L < 0 and current_delta_U < 0:
             print("both -ve sign")
@@ -181,17 +186,20 @@ def braket(start_H_U, start_H_L, T, theta=np.pi/2, phi=0., tol=0.001, N_points =
             # reset lower
             current_delta_L = current_delta_U
             # recalculate Upper
-            current_delta_U = delta(T, current_H_U, theta, phi, N_points =N_points)
+            current_delta_U = delta(
+                T, current_H_U, theta, phi, N_points=N_points)
 
         elif abs(current_delta_L) > abs(current_delta_U):
             old_H_L = current_H_L
             current_H_L = (current_H_L + current_H_U) / 2
-            current_delta_L = delta(T, current_H_L, theta, phi, N_points =N_points)
+            current_delta_L = delta(
+                T, current_H_L, theta, phi, N_points=N_points)
 
         else:
             old_H_U = current_H_U
             current_H_U = (current_H_L + current_H_U) / 2
-            current_delta_U = delta(T, current_H_U, theta, phi, N_points =N_points)
+            current_delta_U = delta(
+                T, current_H_U, theta, phi, N_points=N_points)
 
         iterations += 1
 
@@ -357,6 +365,21 @@ def plot_critical_field():
     critical_fix_200_2000 = np.array([[0.0, 6.5], [14.0674279736905, 6.199999999999999], [20.003552034557174, 5.9], [24.60781252638091, 5.6], [28.492642218926242, 5.300000000000001], [31.919839888820555, 5.0], [35.00234745644323, 4.699999999999999], [37.801837117576945, 4.4], [40.37797076943057, 4.1], [42.75897996579097, 3.8000000000000003], [44.979739255615925, 3.5], [
                                      47.057640091389445, 3.1999999999999997], [49.033707623418024, 2.9000000000000004], [50.93676910430037, 2.5999999999999996], [52.808517191169855, 2.3000000000000003], [54.696692178032606, 2.0], [56.66675496790438, 1.6999999999999997], [58.74033073170307, 1.4000000000000001], [60.60439357336338, 1.0999999999999999], [61.290488493283505, 0.8000000000000003]])
 
+    critical_selected_100_4000 = np.array([[0.0, 6.5], [18.079091186811446, 6.199999999999999], [25.64575405423891, 5.9], [31.533195788232355, 5.6], [36.47454575910953, 5.300000000000001], [40.807779341945555, 5.0], [44.63735722206823, 4.699999999999999], [48.117999226951945, 4.4], [51.25443561318057, 4.1], [54.12982957516597, 3.8000000000000003], [56.779055661865925, 3.5], [
+                                          59.301902786701945, 3.1999999999999997], [61.843766217168024, 2.9000000000000004], [64.94983062773787, 2.5999999999999996], [72.02909824585736, 2.3000000000000003], [79.2333984375, 2.0], [82.42969442102938, 1.6999999999999997], [84.26706413014057, 1.4000000000000001], [85.29341945226963, 1.0999999999999999], [85.7021095870335, 0.8000000000000003]])
+
+    critical_selected_500_10000 = np.array([[[0.0, 6.5], [18.074282956859548, 5.9], [25.73009557642778, 5.300000000000001], [31.761380659568232, 4.699999999999999], [37.01615436318057, 4.1], [
+                                           41.925540036865925, 3.5], [46.946305279668024, 2.9000000000000004], [52.956832620857355, 2.3000000000000003], [62.36865926477938, 1.6999999999999997], [69.42641505773838, 1.0999999999999999]]])
+
+    critical_selected_1000_1000 = np.array([[0.0, 6.5], [13.978626647124734, 6.185], [19.9859555318214, 5.87], [24.743507928266354, 5.555], [28.875728532420524, 5.24], [32.66873980530046, 4.925], [36.22660664836119, 4.609999999999999], [39.598139966039966, 4.295], [42.87931912640704, 3.98], [46.113174357638755, 3.6650000000000005], [
+                                           49.27330168930612, 3.35], [52.465412504079296, 3.0349999999999997], [55.71091120640372, 2.72], [59.038376025125416, 2.405], [62.46622874464707, 2.0900000000000003], [65.96556544884676, 1.775], [69.53687008730391, 1.4599999999999997], [73.41531901223806, 1.145], [79.56635589509291, 0.8299999999999998], [85.06498950028254, 0.5150000000000003]])
+
+    crit_selected_1000_5000 = np.array([[0.0, 6.5], [11.37174469824445, 6.29], [16.250100230922534, 6.08], [19.9859555318214, 5.87], [23.275395774600604, 5.66], [26.181072285375002, 5.45], [28.919673844920524, 5.24], [31.439747844127837, 5.029999999999999], [33.881668229907156, 4.82], [36.22660664836119, 4.609999999999999], [38.515948445701945, 4.4], [40.743604452325435, 4.1899999999999995], [42.92326443890704, 3.98], [45.067311769810324, 3.77], [47.20943594241787, 3.56], [49.33921965805612, 3.35], [51.46849677747653, 3.14], [
+                                       53.5878311541133, 2.93], [55.77682917515372, 2.72], [57.98492853853642, 2.5100000000000002], [60.251754495857355, 2.3000000000000003], [62.55411936964707, 2.0900000000000003], [64.89499190626333, 1.8800000000000003], [67.26104144416645, 1.6699999999999995], [69.65771969667891, 1.4599999999999997], [72.20027561938744, 1.2499999999999998], [75.12950264719204, 1.0399999999999998], [79.91791839509291, 0.8299999999999998], [83.92096716961888, 0.6199999999999999], [99.93546429987978, 0.4099999999999999]])
+
+    crit_3mev_1000_10000 = np.array([[0.0, 6.5], [4.1191906763829085, 6.29], [5.752961596953853, 6.08], [7.004882465535328, 5.87], [8.038183027680649, 5.66], [8.933586124973727, 5.45], [9.736284510332395, 5.24], [10.458990760915285, 5.029999999999999], [11.141458296112368, 4.82], [11.769575682937743, 4.609999999999999], [12.365083901350232, 4.4], [12.953421877831287, 4.1899999999999995], [13.508424620226469, 3.98], [14.05706960044147, 3.77], [14.608009112735171, 3.56], [15.155541650330196, 3.35], [15.72481223345521, 3.14], [
+                                    16.295007118639838, 2.93], [16.884845869915296, 2.72], [17.51569941479852, 2.5100000000000002], [18.166446710573926, 2.3000000000000003], [18.86963481394187, 2.0900000000000003], [19.633552614123673, 1.8800000000000003], [20.46819518926661, 1.6699999999999995], [21.41739414918938, 1.4599999999999997], [22.544166479224714, 1.2499999999999998], [23.924240941212418, 1.0399999999999998], [25.555550730741075, 0.8299999999999998], [27.20555187201036, 0.6199999999999999], [28.576892488627614, 0.4099999999999999]])
+
     fig, ax = plt.subplots(figsize=(5, 5))
     #ax.plot(crit_N_150_F_1000[:, 1], crit_N_150_F_1000[:, 0])
     # ax.plot(crit_N_100_F_1000[:, 1],
@@ -398,25 +421,26 @@ def test_freq_convergence(start, stop, points=10, N_points=100):
     plt.ylabel(r"$\chi^0$")
 
 
-def test_N_convergence(T,N_values=[100,101,102,103], FL=1000):
+def test_N_convergence(T, N_values=[100, 101, 102, 103], FL=1000):
     global FREQUENCY_LIMIT
     global V
     H = []
     temp_freq = FREQUENCY_LIMIT
     FREQUENCY_LIMIT = FL
-    
+
     for n in N_values:
-        print("starting n = "+ str(n))
+        print("starting n = " + str(n))
         V = find_V()
-        H.append(braket(100, 30, T, N_points = n))
-    
+        H.append(braket(100, 30, T, N_points=n))
+
     plt.plot(N_values, H)
-    
+
     # return FL to value before test
     FREQUENCY_LIMIT = temp_freq
 
+
 def find_V(N_points=DEFAULT_N_POINTS):
-    return 1 / Susceptibility(6.5, 0,N_points=N_points)
+    return 1 / Susceptibility(6.5, 0, N_points=N_points)
 
 
 def range_guesser(T):
@@ -427,11 +451,11 @@ def find_phase_diagram(steps=25):
     values = []
     H = 5
     for T_index in range(steps):
-        T = 6 * (1-(T_index) / steps) + .5
+        T = 6.3 * (1-(T_index) / steps) + .2
         H_upper_estimate = range_guesser(T) + 20
         H_lower_estimate = np.clip(range_guesser(T) - 25, -1, 100)
 
-        H = braket(H_upper_estimate, H_lower_estimate,  T)
+        H = braket(H_upper_estimate, 0.5,  T)
         values.append([H, T])
         print("[{}, {}],".format(H, T))
     return np.array(values)
@@ -440,21 +464,21 @@ def find_phase_diagram(steps=25):
 time_0 = time.time()
 # print(delta(6.5, 0))
 
-#V = find_V()
-#print(V)
+V = find_V()
+print(V)
 
 # #print(braket(65, 40, 3))
 
-#test_N_convergence(3)
+# test_N_convergence(3)
 
-#r = find_phase_diagram(10)
+r = find_phase_diagram(30)
 
-#plt.plot(r[:, 1], r[:, 0])
-#print(r.tolist())
+plt.plot(r[:, 1], r[:, 0])
+print(r.tolist())
 
 
-print(Susceptibility(5.5, 62))
-#plot_critical_field()
+#print(Susceptibility(6.5, 0, plot=True))
+# plot_critical_field()
 #test_freq_convergence(1500, 4000, 5)
 
 print("Runtime: {} s".format(time.time() - time_0))
